1651249e18221899ab5dea61c194d441bc476c26
k-petite <katefanchic@mail.ru>
2017-10-19 23:34:43 +0300

    Game over feature was added and tested

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let qwerty = document.getElementById('qwerty');
 let phrase = document.getElementById('phrase');
+
 let missed = 0;
 let overlay = document.getElementById("overlay");
 let keyboard = document.getElementById('qwerty');
@@ -14,17 +15,26 @@ let phrases = [ 'Well nobody is perfect',
 let letterFound;
 let start = document.getElementsByClassName('btn__reset')[0];
 let allLetters = document.getElementsByClassName('letter');
+let h3 = document.getElementsByTagName('h3')[0];
+
+let gameover = document.getElementById("gameover");
+const overall = phrases.length;
 
 let ms = 'hiiii';
 let ul = phrase.getElementsByTagName('ul')[0];
-console.log(ul);
+let score = 0;
+
+
+// console.log(ul);
 
 function getRandomPhraseAsArray(arr){ //gets random phrase, divides it into array and remove it from list of phrases
   let randomphrase = arr[Math.floor(Math.random() * (arr.length-1))]; //gets random phrase
   let used = arr.indexOf(randomphrase); //finds passed phrase to remove it out of phrases array
+  // console.log(arr);
   let split = randomphrase.split(""); //splits passed array
   arr.splice(used, 1); //remove random phrase from phrases array
   // console.log(arr, split);
+
   return split;
 };
 
@@ -44,55 +54,73 @@ function addPhrasetoDisplay(arr) { //put letters of the choosen phrase in LIs an
     }
 };
 
+function gOver() {
+  if (phrases.length === 0) {
+
+    let notif = 'You guessed '+ score +' out of '+ overall +'!';
+    overlay.style.display = 'block';
+    lose.style.display = 'none';
+    win.style.display = 'none';
+    gameover.style.display = 'block';
+    h3.textContent = notif;
+    h3.style.display = 'block';
+    // overlay.appendChild(h3);
+    start.textContent = 'Start again';
+    phrases = [ 'Well nobody is perfect',
+                'Bond James Bond',
+                'Frankly my dear I do not give a damn',
+                'One does not simply walk into Mordor',
+                'Elementary my dear Watson' ];
+    console.log(overlay);
+    // overlay.removeChild(h3);
+  };
+}
+
 
 function reset() {
+  console.log(phrases.length);
+
   let ul = phrase.getElementsByTagName('ul')[0];
   let lis = ul.getElementsByTagName('li');
   let buttons = keyboard.getElementsByTagName('button');
-  // console.log(lis);
-  console.log(lis[0]);
-  missed = 0;
-  for(let i=0; i < lis.length; i++){ //removes all letter created in previous game
-    ul.removeChild(lis[i]);
-    // ul.removeChild(ul.childNodes[i]);
-    // lis[0].parentNode.removeChild(lis[0]);
-    // lis[i].classList.remove("show", 'letter');
-  }
-  console.log(ul);
+  h3.style.display = 'none';
 
-  for(let i=0; i < buttons.length; i++){ //removes all letter created in previous game
+  ul.textContent = "";
+  missed = 0;
+  // console.log(score);
+  gameover.style.display = 'none';
+  // console.log(h3);
+  // h3.textContent = '';
+
+
+
+  for(let i=0; i < buttons.length; i++){ //enables all buttons pressed in previous game
     buttons[i].classList.remove("chosen");
     buttons[i].disabled = false;
-
   }
+
   for(let i=0; i < 5; i++){ //set hearts back to five
-    // var parent = document.getElementsByTagName("ol")[0];
-    // var child = document.createElement("li");
-    // parent.appendChild(child);
-    // console.log(child[i]);
     var parent = document.getElementsByTagName("ol")[0];
     var child = document.getElementsByClassName("tries")[i];
-    // child[i].className = "tries";
     child.style.display = 'inline-block';
   }
   for (let i = 0; i < allLetters.length; i++) {
-    // var letter = allLetters[i].innerHTML;
       allLetters[i].classList.remove("show");
       letterFound = null;
   }
 
-
-
+  gOver();
 
 }
 
 function checkWin() {
   // let win = document.getElementById("win");
-    let win = document.getElementById("win");
-    let lose = document.getElementById("lose");
+    // let win = document.getElementById("win");
+    // let lose = document.getElementById("lose");
     overlay.style.display = 'block';
     lose.style.display = 'none';
     win.style.display = 'block';
+    score++;
     reset();
 }
 
@@ -114,44 +142,42 @@ keyboard.addEventListener('click', function(){
   let show = document.getElementsByClassName('show');
   let letters = document.getElementsByClassName('letter');
   if (button.tagName === 'BUTTON' ) {
-      let letter = button.innerHTML;
-      checkLetter(letter);
-      if (letterFound === null){ //counts mistakes  -should i add ===?
-          missed++;
+    let letter = button.innerHTML;
+    checkLetter(letter);
+    if (letterFound === null){ //counts mistakes  -should i add ===?
+        missed++;
 
-          var parent = document.getElementsByTagName("ol")[0];
-          var child = document.getElementsByClassName("tries")[missed];
-          if (missed < 5){
-            child.style.display = 'none';
-          }
-        };
-      button.className = 'chosen';
-      button.disabled = true;
+        var parent = document.getElementsByTagName("ol")[0];
+        var child = document.getElementsByClassName("tries")[missed];
+        if (missed < 5){
+          child.style.display = 'none';
+        }
+      };
+    button.className = 'chosen';
+    button.disabled = true;
 
-      if (show.length === letters.length){
-        checkWin();
-      }
+    if (show.length === letters.length){
+      checkWin();
+    }
 
+    if (missed === 5){
+      // let lose = document.getElementById("lose");
 
-      if (missed === 5){
-        // let lose = document.getElementById("lose");
-        reset();
-        // let h2 = document.getElementsByClassName("lose")[0];
-        let win = document.getElementById("win");
-        let lose = document.getElementById("lose");
-        overlay.style.display = 'block';
+      // let h2 = document.getElementsByClassName("lose")[0];
+      // let win = document.getElementById("win");
+      // let lose = document.getElementById("lose");
+      overlay.style.display = 'block';
+      win.style.display = 'none';
+      if (phrases.length == 0)
+        {lose.style.display = 'none';
+      } else {
         lose.style.display = 'block';
-        win.style.display = 'none';
-        // button.style.display = 'inline-block';
+      };
+      reset();
+    }
+  }
+});
 
-        // console.log(overlay);
-      }
-
-}});
-
-
-// for(let i=0; i < start.length; i++){
-//  start = document.getElementsByClassName('btn__reset')[i];
 
 
 start.addEventListener("click", function(){ //hides overlay when start game button pressed
